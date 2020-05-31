6a22a5eb6204b456138d1b5597e46c8c8b35d4f0
Rob Galanakis <rob.galanakis@gmail.com>
2020-05-31 12:35:36 -0700

    Add some kronos functions and tests

--- kronos/kronos.go
+++ kronos/kronos.go
@@ -18,3 +18,106 @@ func TMin(t, u time.Time) time.Time {
 	}
 	return u
 }
+
+// Between returns a slice of Times between the given start and end at each interval.
+// start and end are inclusive.
+// If end is before start, nil is returned.
+// If end is earlier than an interval from start, a slice containing only start is returned.
+func Between(start, end time.Time, interval time.Duration) []time.Time {
+	// Pre-allocate the slice. We should be able to exactly know how big the slice needs to be by
+	// dividing the total distance by the interval/step distance.
+	result := makeTimeSlice(int(end.Sub(start)), int(interval))
+	BetweenEach(start, end, interval, func(t time.Time) {
+		result = append(result, t)
+	})
+	return result
+}
+
+// BetweenEach calls each for every time between start and end.
+// See Between for more information.
+func BetweenEach(start, end time.Time, interval time.Duration, each func(time.Time)) {
+	for t := start; !t.After(end); t = t.Add(interval) {
+		each(t)
+	}
+}
+
+// BetweenDates returns a slice of Times between the given start and end dates,
+// adding the given years/months/days between each iteration.
+// start and end are inclusive.
+// If end is before start, nil is returned.
+// If end is earlier than an interval after start, a slice containing only start is returned.
+func BetweenDates(start, end time.Time, offsetYear, offsetMonth, offsetDay int) []time.Time {
+	// Pre-allocate a slice by dividing the the total time/distance in days
+	// by the step in days, and adding 1 for the start date.
+	// We use 31-day months so we allocate the max possible slice by default.
+	// It is likely better to over-allocate by a few, than underestimate and
+	// grow the underlying array which would way overshoot the necessary size.
+	distanceInDays := end.Sub(start) / (24 * time.Hour)
+	stepInDays := (offsetYear * 12 * 30) + (offsetMonth * 30) + offsetDay
+	result := makeTimeSlice(int(distanceInDays), stepInDays)
+
+	BetweenDatesEach(start, end, offsetYear, offsetMonth, offsetDay, func(t time.Time) {
+		result = append(result, t)
+	})
+	return result
+}
+
+// BetweenDatesEach calls each for every time between start and end.
+// See BetweenDates for more information.
+func BetweenDatesEach(start, end time.Time, y, m, d int, each func(time.Time)) {
+	for t := start; !t.After(end); t = t.AddDate(y, m, d) {
+		each(t)
+	}
+}
+
+func makeTimeSlice(totalDuration, stepDuration int) []time.Time {
+	// if end is before start, this can be negative.
+	if totalDuration < 0 {
+		return make([]time.Time, 0)
+	}
+	size := totalDuration / stepDuration
+	// 0 size will need 1 entry for the start, since start is inclusive.
+	size++
+	return make([]time.Time, 0, size)
+}
+
+// DaysInMonth returns the number of days in the month of t.
+func DaysInMonth(t time.Time) int {
+	startOfNextMonth := time.Date(t.Year(), t.Month()+1, 1, 0, 0, 0, 0, t.Location())
+	lastOfThisMonth := startOfNextMonth.AddDate(0, 0, -1)
+	return lastOfThisMonth.Day()
+}
+
+// RollMonth adds months number of months to t (months can be negative).
+// Unlike Go's time.AddDate, this works on a calendar basis.
+// For example, (October 31).AddDate(0, 1, 0) with Go's time package returns (December 1).
+// RollMonth(October 31, 1) returns (November 30).
+func RollMonth(t time.Time, months int) time.Time {
+	offsetDate := t.AddDate(0, months, 0)
+	targetMonth := offsetMonth(t.Month(), months)
+	if offsetDate.Month() != targetMonth {
+		// AddDate may return values that are 'too far ahead' when adding months,
+		// or 'not far enough back' when subtracting months.
+		// In both cases, the calculated date is too far in the future
+		// (in other words: even though the wrongness is in the opposite direction-
+		// too far forward, not enough back- in absolute terms the wrongness is always biased towards the future).
+		// So we can subtract the number of days remaining in the month so we end up in the previous month.
+		offsetDate = offsetDate.AddDate(0, 0, -offsetDate.Day())
+	}
+	return offsetDate
+}
+
+// Get the new month after offsetting month m by offset.
+//
+//     offsetMonth(January, 1) => February
+//     offsetMonth(January, 13) => February
+//     offsetMonth(January, -1) => December
+func offsetMonth(m time.Month, offset int) time.Month {
+	zeroBased := int(m - 1)
+	offsetMonth := zeroBased + offset
+	if offsetMonth < 0 {
+		offsetMonth = 12 + offsetMonth
+	}
+	mod := offsetMonth % 12
+	return time.Month(1 + mod)
+}
---
+++ kronos/kronos_test.go
@@ -0,0 +1,193 @@
+package kronos_test
+
+import (
+	"fmt"
+	"github.com/lithictech/go-aperitif/kronos"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+	"math/rand"
+	"time"
+)
+
+var _ = Describe("kronos.TMin/TMax", func() {
+	Describe("TMin", func() {
+		t1 := time.Now()
+		t2 := t1.Add(time.Hour)
+
+		It("selects the lesser date", func() {
+			Expect(kronos.TMin(t1, t2)).To(BeIdenticalTo(t1))
+		})
+		It("selects the greater date", func() {
+			Expect(kronos.TMin(t1, t2)).To(BeIdenticalTo(t2))
+		})
+	})
+})
+
+var _ = Describe("kronos.Between", func() {
+	It("returns a slice of times between start and end", func() {
+		start := time.Now()
+		end := start.Add(5000 * time.Millisecond)
+		bt := kronos.Between(start, end, 1100*time.Millisecond)
+		Expect(bt).To(HaveLen(5))
+		Expect(bt[0]).To(Equal(start))
+		Expect(bt[1]).To(Equal(start.Add(1100 * time.Millisecond)))
+		Expect(bt[4]).To(Equal(start.Add(4400 * time.Millisecond)))
+	})
+
+	It("is inclusive of start and end", func() {
+		start := time.Now()
+		end := start.Add(5000 * time.Millisecond)
+		bt := kronos.Between(start, end, 1000*time.Millisecond)
+		Expect(bt).To(HaveLen(6))
+	})
+
+	It("contains a single item if start is equal to end", func() {
+		start := time.Now()
+		Expect(kronos.Between(start, start, time.Hour)).To(HaveLen(1))
+	})
+
+	It("contains only start if end is less than interval from start", func() {
+		start := time.Now()
+		Expect(kronos.Between(start, start.Add(59*time.Minute), time.Hour)).To(Equal([]time.Time{start}))
+	})
+
+	It("is empty if end is after start", func() {
+		start := time.Now()
+		Expect(kronos.Between(start, start.Add(-20*time.Hour), time.Hour)).To(BeEmpty())
+	})
+
+	It("pre-allocates a slice of the correct size", func() {
+		start := time.Now()
+		bt := kronos.Between(start, start.Add(5000*time.Millisecond), 1100*time.Millisecond)
+		Expect(bt).To(HaveLen(5))
+		Expect(bt).To(HaveCap(5))
+	})
+})
+
+var _ = Describe("kronos.RollMonths", func() {
+	date := func(y, m, d int) time.Time {
+		return time.Date(y, time.Month(m), d, 0, 0, 0, 0, time.UTC)
+	}
+
+	ymd := func(t time.Time) []int {
+		return []int{t.Year(), int(t.Month()), t.Day()}
+	}
+
+	entry := func(arg time.Time, offset int, expected time.Time, note string) TableEntry {
+		desc := fmt.Sprintf("%d/%d + %d => %d/%d (%s)",
+			arg.Month(), arg.Day(), offset, expected.Month(), expected.Day(), note)
+		return Entry(desc, arg, offset, expected)
+	}
+
+	DescribeTable("rolls months",
+		func(arg time.Time, offset int, expected time.Time) {
+			actual := kronos.RollMonth(arg, offset)
+			Expect(ymd(actual)).To(Equal(ymd(expected)))
+		},
+		entry(date(2016, 8, 30), 1, date(2016, 9, 30), "forward a month"),
+		entry(date(2016, 3, 31), 1, date(2016, 4, 30), "April has fewer days than March"),
+		entry(date(2016, 3, 31), 2, date(2016, 5, 31), "through April, with fewer days"),
+		entry(date(2016, 3, 31), 12, date(2017, 3, 31), "forward 12 months"),
+		entry(date(2016, 12, 31), 1, date(2017, 1, 31), "forward over year boundary"),
+		entry(date(2016, 1, 31), 1, date(2016, 2, 29), "forward into a shorter month"),
+		entry(date(2016, 1, 31), 2, date(2016, 3, 31), "forward over a shorter month"),
+
+		entry(date(2016, 9, 30), -1, date(2016, 8, 30), "back a month"),
+		entry(date(2016, 3, 31), -1, date(2016, 2, 29), "leap feb has fewer days than march"),
+		entry(date(2015, 3, 31), -1, date(2015, 2, 28), "nonleap feb has fewer days than march"),
+		entry(date(2016, 3, 31), -2, date(2016, 1, 31), "through feb with fewer days"),
+		entry(date(2016, 3, 31), -3, date(2015, 12, 31), "over year boundary"),
+		entry(date(2016, 3, 31), -12, date(2015, 3, 31), "back 12 months"),
+	)
+})
+
+var _ = Describe("kronos.DaysInMonth", func() {
+	DescribeTable("returns the number of days in the month described by the given time",
+		func(y, m, expected int) {
+			t := time.Date(y, time.Month(m), rand.Intn(25)+1, 0, 0, 0, 0, time.UTC)
+			Expect(kronos.DaysInMonth(t)).To(Equal(expected))
+		},
+		Entry("Jan has 31 days", 2015, 1, 31),
+		Entry("Dec has 31 days", 2015, 12, 31),
+		Entry("June has 30 days", 2015, 6, 30),
+		Entry("Jan has 31 days", 2015, 1, 31),
+		Entry("March has 31 days", 2015, 3, 31),
+		Entry("Regular Feb has 28 days", 2015, 2, 28),
+		Entry("Leap year Feb has 29 days", 2016, 2, 29),
+	)
+})
+
+func ExampleBetween() {
+	sta, _ := time.Parse(time.RFC3339, "2006-01-02T15:04:05Z")
+	end, _ := time.Parse(time.RFC3339, "2006-01-02T15:05:05Z")
+	bt := kronos.Between(sta, end, 28*time.Second)
+	fmt.Println("Len:", len(bt))
+	for _, t := range bt {
+		fmt.Println(t.Format("15:04:05"))
+	}
+	// Output:
+	// Len: 3
+	// 15:04:05
+	// 15:04:33
+	// 15:05:01
+}
+
+var _ = Describe("kronos.BetweenDates", func() {
+	It("returns a slice of times between start and end", func() {
+		start := time.Now()
+		end := start.AddDate(0, 0, 5)
+		bt := kronos.BetweenDates(start, end, 0, 0, 2)
+		Expect(bt).To(HaveLen(3))
+		Expect(bt[0]).To(Equal(start))
+		Expect(bt[1]).To(Equal(start.AddDate(0, 0, 2)))
+		Expect(bt[2]).To(Equal(start.AddDate(0, 0, 4)))
+	})
+
+	It("is inclusive of start and end", func() {
+		start := time.Now()
+		end := start.AddDate(0, 0, 2)
+		bt := kronos.BetweenDates(start, end, 0, 0, 2)
+		Expect(bt).To(HaveLen(2))
+		Expect(bt[0]).To(Equal(start))
+		Expect(bt[1]).To(Equal(end))
+	})
+
+	It("contains a single item if start is equal to end", func() {
+		start := time.Now()
+		Expect(kronos.BetweenDates(start, start, 1, 1, 1)).To(HaveLen(1))
+	})
+
+	It("contains only start if end is less than interval from start", func() {
+		start := time.Now()
+		end := start.AddDate(0, 0, 5)
+		Expect(kronos.BetweenDates(start, end, 0, 1, 0)).To(Equal([]time.Time{start}))
+	})
+
+	It("is empty if end is after start", func() {
+		start := time.Now()
+		Expect(kronos.BetweenDates(start, start.Add(time.Hour*24*60*-1), 0, 0, 1)).To(BeEmpty())
+	})
+
+	It("pre-allocates a slice of the correct size", func() {
+		start := time.Now()
+		bt := kronos.BetweenDates(start, start.AddDate(5, 0, 0), 1, 1, 1)
+		Expect(bt).To(HaveLen(5))
+		Expect(bt).To(HaveCap(5))
+	})
+})
+
+func ExampleBetweenDates() {
+	start, _ := time.Parse("2006-01-02", "2012-11-22")
+	end, _ := time.Parse("2006-01-02", "2015-03-06")
+	bt := kronos.BetweenDates(start, end, 1, 0, 0)
+	fmt.Println("Len:", len(bt))
+	for _, t := range bt {
+		fmt.Println(t.Format("2006-01-02"))
+	}
+	// Output:
+	// Len: 3
+	// 2012-11-22
+	// 2013-11-22
+	// 2014-11-22
+}
